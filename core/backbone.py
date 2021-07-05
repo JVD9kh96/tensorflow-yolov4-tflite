931a4026900e590fdd0b9063bae048928a4a70cd
JVD9kh96 <J.khorramdel96@gmail.com>
2021-07-05 20:55:54 +0000

    vit-v1 and YOLO_vit_v1 has been added

--- core/backbone.py
+++ core/backbone.py
@@ -3,6 +3,70 @@
 
 import tensorflow as tf
 import core.common as common
+from tensorflow.keras import layers
+import numpy as np
+from tensorflow import keras
+import tensorflow_addons as tfa
+
+
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = layers.Dropout(dropout_rate)(x)
+    return x
+
+class Patches(layers.Layer):
+    def __init__(self, patch_size):
+        super(Patches, self).__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+
+class Patches_sp(layers.Layer):
+    def __init__(self, patch_size):
+        super(Patches_sp, self).__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size[0], self.patch_size[1], 1],
+            strides=[1, self.patch_size[0], self.patch_size[1], 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches
+
+
+
+class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super(PatchEncoder, self).__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        encoded = self.projection(patch) + self.position_embedding(positions)
+        return encoded
+
 
 def darknet53(input_data):
 
@@ -165,3 +229,39 @@ def darknet53_tiny(input_data):
     return route_1, input_data
 
 
+
+def VIT_v1(inputs, image_size = 416,
+                          patch_size=8,
+                          projection_dim = 128,
+                          transformer_layers =[6, 6, 6],
+                          attention_heads=[4, 4, 4]):
+    
+    num_patches = (image_size // patch_size) ** 2
+    transformer_units = [projection_dim * 2, projection_dim] 
+    # inputs = layers.Input(shape=(image_size, image_size, 3))
+    patches = Patches(patch_size)(inputs)
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)    
+    encoded_patches = common.transformer(encoded_patches, projection_dim, transformer_units, transformer_layers[0], num_heads = attention_heads[0])
+    encoded_patches = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    route1 = encoded_patches
+    route1 = tf.keras.layers.Reshape((image_size//8, image_size//8, projection_dim))(route1)
+    dict_size = getattr(encoded_patches, 'shape')
+    encoded_patches = tf.expand_dims(encoded_patches, axis = -1)
+    encoded_patches = Patches_sp([4, projection_dim])(encoded_patches)
+    # if not down_sample[0]:
+    encoded_patches = PatchEncoder(dict_size[1]//4, projection_dim*2)(encoded_patches) 
+    # else:
+    #     encoded_patches = tf.keras.layers.MaxPooling1D(pool_size = 4, strides=4)(encoded_patches)
+
+    encoded_patches = common.transformer(encoded_patches, projection_dim*2, [projection_dim*4, projection_dim*2], transformer_layers[1], num_heads = attention_heads[1])
+    encoded_patches = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    route2 = encoded_patches
+    route2 = tf.keras.layers.Reshape((image_size//16, image_size//16, projection_dim*2))(route2)
+    encoded_patches = tf.expand_dims(encoded_patches, axis = -1)
+    encoded_patches = Patches_sp([4, projection_dim*2])(encoded_patches)
+    encoded_patches = PatchEncoder(dict_size[1]//16, projection_dim*4)(encoded_patches)    
+    encoded_patches = common.transformer(encoded_patches, projection_dim*4, [projection_dim*8, projection_dim*4], transformer_layers[2], num_heads = attention_heads[2])
+    encoded_patches = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+    encoded_patches = tf.keras.layers.Reshape((image_size//32, image_size//32, projection_dim*4))(encoded_patches)
+    # model = keras.Model(inputs=inputs, outputs=[route1, route2, encoded_patches])
+    return route1, route2, encoded_patches
--- core/common.py
+++ core/common.py
@@ -2,6 +2,9 @@
 # coding=utf-8
 
 import tensorflow as tf
+from tensorflow.keras import layers
+import tensorflow_addons as tfa
+
 # import tensorflow_addons as tfa
 class BatchNormalization(tf.keras.layers.BatchNormalization):
     """
@@ -65,3 +68,27 @@ def route_group(input_layer, groups, group_id):
 def upsample(input_layer):
     return tf.image.resize(input_layer, (input_layer.shape[1] * 2, input_layer.shape[2] * 2), method='bilinear')
 
+def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = layers.Dropout(dropout_rate)(x)
+    return x
+
+def transformer(input_layer, projection_dim, transformer_units, num_layers = 4, num_heads = 4):
+    encoded_patches = input_layer
+    for _ in range(num_layers):
+        # Layer normalization 1.
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        # Create a multi-head attention layer.
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+        )(x1, x1)
+        # Skip connection 1.
+        x2 = layers.Add()([attention_output, encoded_patches])
+        # Layer normalization 2.
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        # MLP.
+        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+        # Skip connection 2.
+        encoded_patches = layers.Add()([x3, x2])
+    return encoded_patches
