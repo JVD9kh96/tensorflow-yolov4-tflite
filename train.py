6a8811312c866b8b1c132f2040fbebba00e37ff5
Javad <54026703+JVD9kh96@users.noreply.github.com>
2021-07-07 04:47:05 +0430

    Update train.py
    bugs fixed

--- train.py
+++ train.py
@@ -3,9 +3,9 @@ from absl.flags import FLAGS
 import os
 import shutil
 import tensorflow as tf
-    physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    if len(physical_devices) > 0:
-        tf.config.experimental.set_memory_growth(physical_devices[0], True)
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
         
 from core.yolov4 import YOLO, decode, compute_loss, decode_train
 from core.dataset import Dataset
