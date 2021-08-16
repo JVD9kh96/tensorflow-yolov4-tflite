d966ecbedf0636702724ad6c91cd8c71537d7548
Ahmadmoo <66487994+Ahmadmoo@users.noreply.github.com>
2021-08-16 21:34:19 +0430

    Update config.py

--- core/config.py
+++ core/config.py
@@ -27,7 +27,7 @@ __C.YOLO.IOU_LOSS_THRESH      = 0.5
 __C.TRAIN                     = edict()
 
 __C.TRAIN.ANNOT_PATH          = "data/dataset/voc_train.txt"
-__C.TRAIN.BATCH_SIZE          = 2
+__C.TRAIN.BATCH_SIZE          = 6
 # __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
 __C.TRAIN.INPUT_SIZE          = 416
 __C.TRAIN.DATA_AUG            = True
