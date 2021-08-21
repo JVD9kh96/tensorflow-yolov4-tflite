ab46625662bde38459ea1e4dcf291d47847159c1
Javad <54026703+JVD9kh96@users.noreply.github.com>
2021-08-22 03:33:20 +0430

    Update yolov4.py

--- core/yolov4.py
+++ core/yolov4.py
@@ -454,56 +454,56 @@ def YOLOv4_att_v4(input_layer,
                                                           normalization = normal)
 
     route = conv
-    conv = common.convolutional(conv, (1, 1, 512, 256), norm = 1)
+    conv = common.convolutional(conv, (1, 1, 512, 256))
     conv = common.upsample(conv)
-    route_2 = common.convolutional(route_2, (1, 1, 512, 256), norm = 1)
+    route_2 = common.convolutional(route_2, (1, 1, 512, 256))
     conv = tf.concat([route_2, conv], axis=-1)
 
-    conv = common.convolutional(conv, (1, 1, 512, 256), norm = 1)
-    conv = common.convolutional(conv, (3, 3, 256, 512), norm = 1)
-    conv = common.convolutional(conv, (1, 1, 512, 256), norm = 1)
-    conv = common.convolutional(conv, (3, 3, 256, 512), norm = 1)
-    conv = common.convolutional(conv, (1, 1, 512, 256), norm = 1)
+    conv = common.convolutional(conv, (1, 1, 512, 256))
+    conv = common.convolutional(conv, (3, 3, 256, 512))
+    conv = common.convolutional(conv, (1, 1, 512, 256))
+    conv = common.convolutional(conv, (3, 3, 256, 512))
+    conv = common.convolutional(conv, (1, 1, 512, 256))
 
     route_2 = conv
-    conv = common.convolutional(conv, (1, 1, 256, 128), norm = 1)
+    conv = common.convolutional(conv, (1, 1, 256, 128))
     conv = common.upsample(conv)
-    route_1 = common.convolutional(route_1, (1, 1, 256, 128), norm = 1)
+    route_1 = common.convolutional(route_1, (1, 1, 256, 128))
     conv = tf.concat([route_1, conv], axis=-1)
 
-    conv = common.convolutional(conv, (1, 1, 256, 128), norm = 1)
-    conv = common.convolutional(conv, (3, 3, 128, 256), norm = 1)
-    conv = common.convolutional(conv, (1, 1, 256, 128), norm = 1)
-    conv = common.convolutional(conv, (3, 3, 128, 256), norm = 1)
-    conv = common.convolutional(conv, (1, 1, 256, 128), norm = 1)
+    conv = common.convolutional(conv, (1, 1, 256, 128))
+    conv = common.convolutional(conv, (3, 3, 128, 256))
+    conv = common.convolutional(conv, (1, 1, 256, 128))
+    conv = common.convolutional(conv, (3, 3, 128, 256))
+    conv = common.convolutional(conv, (1, 1, 256, 128))
 
     route_1 = conv
-    conv = common.convolutional(conv, (3, 3, 128, 256), norm = 1)
+    conv = common.convolutional(conv, (3, 3, 128, 256))
     conv_sbbox = common.convolutional(conv, (1, 1, 256, 3 * (NUM_CLASS + 5)), activate=False, bn=False)
 
-    conv = common.convolutional(route_1, (3, 3, 128, 256), downsample=True, norm = 1)
+    conv = common.convolutional(route_1, (3, 3, 128, 256), downsample=True)
     conv = tf.concat([conv, route_2], axis=-1)
 
-    conv = common.convolutional(conv, (1, 1, 512, 256), norm = 1)
-    conv = common.convolutional(conv, (3, 3, 256, 512), norm = 1)
-    conv = common.convolutional(conv, (1, 1, 512, 256), norm = 1)
-    conv = common.convolutional(conv, (3, 3, 256, 512), norm = 1)
-    conv = common.convolutional(conv, (1, 1, 512, 256), norm = 1)
+    conv = common.convolutional(conv, (1, 1, 512, 256))
+    conv = common.convolutional(conv, (3, 3, 256, 512))
+    conv = common.convolutional(conv, (1, 1, 512, 256))
+    conv = common.convolutional(conv, (3, 3, 256, 512))
+    conv = common.convolutional(conv, (1, 1, 512, 256))
 
     route_2 = conv
-    conv = common.convolutional(conv, (3, 3, 256, 512), norm = 1)
+    conv = common.convolutional(conv, (3, 3, 256, 512))
     conv_mbbox = common.convolutional(conv, (1, 1, 512, 3 * (NUM_CLASS + 5)), activate=False, bn=False)
 
-    conv = common.convolutional(route_2, (3, 3, 256, 512), downsample=True, norm = 1)
+    conv = common.convolutional(route_2, (3, 3, 256, 512), downsample=True)
     conv = tf.concat([conv, route], axis=-1)
 
-    conv = common.convolutional(conv, (1, 1, 1024, 512), norm = 1)
-    conv = common.convolutional(conv, (3, 3, 512, 1024), norm = 1)
-    conv = common.convolutional(conv, (1, 1, 1024, 512), norm = 1)
-    conv = common.convolutional(conv, (3, 3, 512, 1024), norm = 1)
-    conv = common.convolutional(conv, (1, 1, 1024, 512), norm = 1)
+    conv = common.convolutional(conv, (1, 1, 1024, 512))
+    conv = common.convolutional(conv, (3, 3, 512, 1024))
+    conv = common.convolutional(conv, (1, 1, 1024, 512))
+    conv = common.convolutional(conv, (3, 3, 512, 1024))
+    conv = common.convolutional(conv, (1, 1, 1024, 512))
 
-    conv = common.convolutional(conv, (3, 3, 512, 1024), norm = 1)
+    conv = common.convolutional(conv, (3, 3, 512, 1024))
     conv_lbbox = common.convolutional(conv, (1, 1, 1024, 3 * (NUM_CLASS + 5)), activate=False, bn=False)
 
     return [conv_sbbox, conv_mbbox, conv_lbbox]
