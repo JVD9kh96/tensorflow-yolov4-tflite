31b739d11de9d84038d81543e1eb1c012ddc3db9
Armin Ghanbarzadeh <agz1986@gmail.com>
2021-09-04 23:19:15 +0430

    Update dataset.py

--- core/dataset.py
+++ core/dataset.py
@@ -339,6 +339,9 @@ class Dataset(object):
                 self.num_classes, 1.0 / self.num_classes
             )
             deta = tf.constant(0.01, dtype=tf.float64)
+            print(onehot)
+            print(deta)
+            print(uniform_distribution)
             smooth_onehot = onehot * (1 - deta) + deta * uniform_distribution
 
             bbox_xywh = tf.concat(
