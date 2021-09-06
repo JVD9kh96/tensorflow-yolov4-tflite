f4d7e866540a1811e135c94ad511382cefda2e3f
Armin Ghanbarzadeh <agz1986@gmail.com>
2021-09-06 20:53:00 +0430

    Update dataset.py

--- core/dataset.py
+++ core/dataset.py
@@ -387,7 +387,8 @@ class Dataset(object):
                     exist_positive = True
 
             if not exist_positive:
-                best_anchor_ind = tf.math.argmax(tf.constant(iou).reshape(-1), axis=-1)
+                print(iou)
+                best_anchor_ind = tf.math.argmax(tf.Variable(iou).reshape(-1), axis=-1)
                 best_detect = int(best_anchor_ind / self.anchor_per_scale)
                 best_anchor = int(best_anchor_ind % self.anchor_per_scale)
                 xind, yind = tf.cast(tf.math.floor(bbox_xywh_scaled[best_detect, 0:2]), tf.int32)
