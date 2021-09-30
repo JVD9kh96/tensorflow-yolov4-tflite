b92b54590fa620ce27f770fa420df89577058eb4
Javad <54026703+JVD9kh96@users.noreply.github.com>
2021-09-30 05:41:40 +0330

    Update common.py

--- core/common.py
+++ core/common.py
@@ -32,7 +32,7 @@ class DropBlock2D(tf.keras.layers.Layer):
         self.set_keep_prob()
         super(DropBlock2D, self).build(input_shape)
 
-    def call(self, inputs, training=None, **kwargs):
+    def call(self, inputs, training=False):
         def drop():
             mask = self._create_mask(tf.shape(inputs))
             output = inputs * mask
@@ -40,11 +40,9 @@ class DropBlock2D(tf.keras.layers.Layer):
                              true_fn=lambda: output * tf.cast(tf.size(mask), tf.float32) / tf.reduce_sum(mask),
                              false_fn=lambda: output)
             return output
-
-        if training is None:
-            training = K.learning_phase()
-        training = tf.cast(training, tf.bool)
-        output = tf.cond(tf.logical_or(tf.logical_not(training), tf.equal(self.keep_prob, 1.0)),
+        
+        training = tf.cast(training, tf.float32)
+        output = tf.cond(tf.equal(1.0-training, 1.0), tf.equal(self.keep_prob, 1.0)),
                          true_fn=lambda: inputs,
                          false_fn=drop)
         return output
