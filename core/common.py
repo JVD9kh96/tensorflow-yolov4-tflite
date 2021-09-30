3f985834048c8add7c0be363d0f170fbd8ef42dd
Javad <54026703+JVD9kh96@users.noreply.github.com>
2021-09-30 04:06:56 +0330

    Update common.py

--- core/common.py
+++ core/common.py
@@ -5,14 +5,66 @@ import tensorflow as tf
 from tensorflow.keras import layers
 # import tensorflow_addons as tfa
 from tensorflow.keras import regularizers
+from tensorflow.python.keras import backend as K
 
-# import tensorflow_addons as tfa
-def ws_reg(kernel):
-    kernel_mean = tf.math.reduce_mean(kernel, axis=[0, 1, 2], keepdims=True, name='kernel_mean')
-    kernel = kernel - kernel_mean
-    kernel_std = tf.keras.backend.std(kernel, axis=[0, 1, 2], keepdims=True)
-    kernel = kernel / (kernel_std + 1e-5)
-    return kernel
+def _bernoulli(shape, mean):
+    return tf.nn.relu(tf.sign(mean - tf.random.uniform(shape, minval=0, maxval=1, dtype=tf.float32)))
+
+class DropBlock2D(tf.keras.layers.Layer):
+    def __init__(self, keep_prob, block_size, scale=True, **kwargs):
+        super(DropBlock2D, self).__init__(**kwargs)
+        self.keep_prob = float(keep_prob) if isinstance(keep_prob, int) else keep_prob
+        self.block_size = int(block_size)
+        self.scale = tf.constant(scale, dtype=tf.bool) if isinstance(scale, bool) else scale
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def build(self, input_shape):
+        assert len(input_shape) == 4
+        _, self.h, self.w, self.channel = input_shape.as_list()
+        # pad the mask
+        p1 = (self.block_size - 1) // 2
+        p0 = (self.block_size - 1) - p1
+        self.padding = [[0, 0], [p0, p1], [p0, p1], [0, 0]]
+        self.set_keep_prob()
+        super(DropBlock2D, self).build(input_shape)
+
+    def call(self, inputs, training=None, **kwargs):
+        def drop():
+            mask = self._create_mask(tf.shape(inputs))
+            output = inputs * mask
+            output = tf.cond(self.scale,
+                             true_fn=lambda: output * tf.cast(tf.size(mask), tf.float32) / tf.reduce_sum(mask),
+                             false_fn=lambda: output)
+            return output
+
+        if training is None:
+            training = K.learning_phase()
+            training = tf.cast(training, tf.bool)
+        output = tf.cond(tf.logical_or(tf.logical_not(training), tf.equal(self.keep_prob, 1.0)),
+                         true_fn=lambda: inputs,
+                         false_fn=drop)
+        return output
+
+    def set_keep_prob(self, keep_prob=None):
+        """This method only supports Eager Execution"""
+        if keep_prob is not None:
+            self.keep_prob = keep_prob
+        w, h = tf.cast(self.w, tf.float32), tf.cast(self.h, tf.float32)
+        self.gamma = (1. - self.keep_prob) * (w * h) / (self.block_size ** 2) / \
+                     ((w - self.block_size + 1) * (h - self.block_size + 1))
+
+    def _create_mask(self, input_shape):
+        sampling_mask_shape = tf.stack([input_shape[0],
+                                       self.h - self.block_size + 1,
+                                       self.w - self.block_size + 1,
+                                       self.channel])
+        mask = _bernoulli(sampling_mask_shape, self.gamma)
+        mask = tf.pad(mask, self.padding)
+        mask = tf.nn.max_pool(mask, [1, self.block_size, self.block_size, 1], [1, 1, 1, 1], 'SAME')
+        mask = 1 - mask
+        return mask
 
 class BatchNormalization(tf.keras.layers.BatchNormalization):
     """
@@ -43,6 +95,9 @@ def convolutional(input_layer, filters_shape, downsample=False, activate=True, b
 
     if bn and norm==0: 
         conv = BatchNormalization()(conv)
+        conv_shape = tf.shape(conv)
+        block_size = tf.maximum(1, conv_shape[1] // 32)
+        conv = DropBlock2D(keep_prob=0.9, block_size=block_size)(conv)
     # elif bn and norm==1:
     #     conv = tfa.layers.GroupNormalization(groups = min(filters_shape[-1], 32))(conv)
      
@@ -238,6 +293,9 @@ def transformer_block(inp,
 
     if normalization == 'batch':
         x1 = tf.keras.layers.BatchNormalization()(inp)
+        conv_shape = tf.shape(x1)
+        block_size = tf.maximum(1, conv_shape[1] // 32)
+        x1 = DropBlock2D(keep_prob=0.9, block_size=block_size)(x1)
     # elif normalization == 'group':
     #     x1 = tfa.layers.GroupNormalization(min(16, inp.shape[-1]))(inp)
     elif normalization == 'layer':
@@ -254,6 +312,9 @@ def transformer_block(inp,
     x3 = tf.keras.layers.Add()([x2, inp])
     if normalization == 'batch':
         x4 = tf.keras.layers.BatchNormalization()(x3)
+        conv_shape = tf.shape(x4)
+        block_size = tf.maximum(1, conv_shape[1] // 32)
+        x4 = DropBlock2D(keep_prob=0.9, block_size=block_size)(x4)
     # elif normalization == 'group':
     #     x4 = tfa.layers.GroupNormalization(min(16, x3.shape[-1]))(x3)
     elif normalization == 'layer':
@@ -298,6 +359,9 @@ def transformer_block(inp,
 
     if normalization == 'batch':
         x8 = tf.keras.layers.BatchNormalization()(x8)
+        conv_shape = tf.shape(x8)
+        block_size = tf.maximum(1, conv_shape[1] // 32)
+        x8 = DropBlock2D(keep_prob=0.9, block_size=block_size)(x8) 
     # elif normalization == 'group':
     #     x8 = tfa.layers.GroupNormalization(min(16, x3.shape[-1]))(x8)
     elif normalization == 'layer':
