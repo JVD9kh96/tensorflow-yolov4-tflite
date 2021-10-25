c008fad430b4168c07c11697c5f6c7ba2c71b41e
Javad <54026703+JVD9kh96@users.noreply.github.com>
2021-10-25 17:23:49 +0330

    Update common.py

--- core/common.py
+++ core/common.py
@@ -89,8 +89,8 @@ def route_group(input_layer, groups, group_id):
     return convs[group_id]
 
 def upsample(input_layer, dtype=None):
-    if dtype is None:
-        dtype = input_layer.dtype
+#     if dtype is None:
+#         dtype = input_layer.dtype
     return tf.cast(tf.image.resize(input_layer, (input_layer.shape[1] * 2, input_layer.shape[2] * 2), method='bilinear'), dtype)
 
 def mlp(x, hidden_units, dropout_rate, activation = 'gelu'):
