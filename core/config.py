a87d1f7b7836e05309096b148143569fe611a99a
sepidehetaati <78643780+sepidehetaati@users.noreply.github.com>
2022-07-14 15:48:02 +0430

    Update config.py

--- core/config.py
+++ core/config.py
@@ -27,15 +27,15 @@ __C.YOLO.IOU_LOSS_THRESH      = 0.5
 __C.TRAIN                     = edict()
 
 __C.TRAIN.ANNOT_PATH          = "./data/dataset/Annotations_Train.txt"
-__C.TRAIN.BATCH_SIZE          = 1
+__C.TRAIN.BATCH_SIZE          = 2
 # __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
 __C.TRAIN.INPUT_SIZE          = 704
 __C.TRAIN.DATA_AUG            = True
 __C.TRAIN.LR_INIT             = 1e-3
 __C.TRAIN.LR_END              = 1e-6
 __C.TRAIN.WARMUP_EPOCHS       = 3
-__C.TRAIN.FISRT_STAGE_EPOCHS    = 12
-__C.TRAIN.SECOND_STAGE_EPOCHS   = 15
+__C.TRAIN.FISRT_STAGE_EPOCHS    = 5
+__C.TRAIN.SECOND_STAGE_EPOCHS   = 10
 
 
 
@@ -43,7 +43,7 @@ __C.TRAIN.SECOND_STAGE_EPOCHS   = 15
 __C.TEST                      = edict()
 
 __C.TEST.ANNOT_PATH           = "./data/dataset/Annotations_Test.txt"
-__C.TEST.BATCH_SIZE           = 1
+__C.TEST.BATCH_SIZE           = 2
 __C.TEST.INPUT_SIZE           = 704
 __C.TEST.DATA_AUG             = False
 __C.TEST.DECTECTED_IMAGE_PATH = "./data/detection/"
