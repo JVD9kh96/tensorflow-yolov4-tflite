d2dd251cd813879a4f0778546813728562c83bbf
sepidehetaati <78643780+sepidehetaati@users.noreply.github.com>
2022-07-14 21:49:25 +0430

    Update config.py

--- core/config.py
+++ core/config.py
@@ -27,9 +27,9 @@ __C.YOLO.IOU_LOSS_THRESH      = 0.5
 __C.TRAIN                     = edict()
 
 __C.TRAIN.ANNOT_PATH          = "./data/dataset/Annotations_Train.txt"
-__C.TRAIN.BATCH_SIZE          = 4
+__C.TRAIN.BATCH_SIZE          = 16
 # __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
-__C.TRAIN.INPUT_SIZE          = 512
+__C.TRAIN.INPUT_SIZE          = 416
 __C.TRAIN.DATA_AUG            = True
 __C.TRAIN.LR_INIT             = 1e-4
 __C.TRAIN.LR_END              = 1e-6
@@ -43,8 +43,8 @@ __C.TRAIN.SECOND_STAGE_EPOCHS   = 12
 __C.TEST                      = edict()
 
 __C.TEST.ANNOT_PATH           = "./data/dataset/Annotations_Test.txt"
-__C.TEST.BATCH_SIZE           = 4
-__C.TEST.INPUT_SIZE           = 512
+__C.TEST.BATCH_SIZE           = 16
+__C.TEST.INPUT_SIZE           = 416
 __C.TEST.DATA_AUG             = False
 __C.TEST.DECTECTED_IMAGE_PATH = "./data/detection/"
 __C.TEST.SCORE_THRESHOLD      = 0.25
