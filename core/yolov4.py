eda15dccaa2c47179b2fda35450a7a9e067944ec
JVD9kh96 <J.khorramdel96@gmail.com>
2022-10-17 10:05:27 +0000

    compute loss modified

--- core/yolov4.py
+++ core/yolov4.py
@@ -427,5 +427,51 @@ def compute_loss(pred, conv, label, bboxes, STRIDES, NUM_CLASS, IOU_LOSS_THRESH,
 
 
 
+def compute_loss_cond(pred, conv, label, bboxes, STRIDES, NUM_CLASS, IOU_LOSS_THRESH, i=0):
+    conv_shape  = tf.shape(conv)
+    batch_size  = conv_shape[0]
+    output_size = conv_shape[1]
+    input_size  = STRIDES[i] * output_size
+    conv = tf.reshape(conv, (batch_size, output_size, output_size, 3, 5 + NUM_CLASS))
 
+    conv_raw_conf       = conv[:, :, :, :, 4:5]
+    conv_raw_prior_prob = conv[:, :, :, :, 5:cfg.COND.SMOOTH_PRIOR+5]
+    conv_raw_post_prob  = conv[:, :, :, :, cfg.COND.SMOOTH_PRIOR+5:]
+
+    pred_xywh     = pred[:, :, :, :, 0:4]
+    pred_conf     = pred[:, :, :, :, 4:5]
+
+    label_xywh          = label[:, :, :, :, 0:4]
+    respond_bbox        = label[:, :, :, :, 4:5]
+    label_prior_prob    = label[:, :, :, :, 5:cfg.COND.SMOOTH_PRIOR+5]
+    label_post_prob     = label[:, :, :, :, cfg.COND.SMOOTH_PRIOR+5:]
+
+    giou = tf.expand_dims(utils.bbox_giou(pred_xywh, label_xywh), axis=-1)
+    input_size = tf.cast(input_size, tf.float32)
+
+    bbox_loss_scale = 2.0 - 1.0 * label_xywh[:, :, :, :, 2:3] * label_xywh[:, :, :, :, 3:4] / (input_size ** 2)
+    giou_loss = respond_bbox * bbox_loss_scale * (1- giou)
+
+    iou = utils.bbox_iou(pred_xywh[:, :, :, :, np.newaxis, :], bboxes[:, np.newaxis, np.newaxis, np.newaxis, :, :])
+    max_iou = tf.expand_dims(tf.reduce_max(iou, axis=-1), axis=-1)
+
+    respond_bgd = (1.0 - respond_bbox) * tf.cast( max_iou < IOU_LOSS_THRESH, tf.float32 )
+
+    conf_focal = tf.pow(respond_bbox - pred_conf, 2)
+
+    conf_loss = conf_focal * (
+            respond_bbox * tf.nn.sigmoid_cross_entropy_with_logits(labels=respond_bbox, logits=conv_raw_conf)
+            +
+            respond_bgd * tf.nn.sigmoid_cross_entropy_with_logits(labels=respond_bbox, logits=conv_raw_conf)
+    )
+
+    prior_prob_loss = respond_bbox * tf.nn.sigmoid_cross_entropy_with_logits(labels=label_prior_prob, logits=conv_raw_prior_prob)
+    post_prob_loss  = 0.0
+    
+
+    giou_loss       = tf.reduce_mean(tf.reduce_sum(giou_loss, axis=[1,2,3,4]))
+    conf_loss       = tf.reduce_mean(tf.reduce_sum(conf_loss, axis=[1,2,3,4]))
+    prior_prob_loss = tf.reduce_mean(tf.reduce_sum(prior_prob_loss, axis=[1,2,3,4]))
+
+    return giou_loss, conf_loss, prob_loss
 
