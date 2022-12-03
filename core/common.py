70c379ac9307de223e771fe2e7a8c8cea24218e1
Javad <54026703+JVD9kh96@users.noreply.github.com>
2022-12-03 23:31:57 +0330

    Update common.py

--- core/common.py
+++ core/common.py
@@ -162,48 +162,49 @@ class conv_prod(tf.keras.layers.Layer):
         shape  = feature_map_2.shape.as_list()
         kshape = feature_map_1.shape.as_list()
         MB     = shape[0]
-        kernel = tf.reshape(kernel, [MB,
-                                     self.filter_size[0],
-                                     self.filter_size[1],
-                                     kshape[-1],
-                                     (kshape[1]//self.filter_size[0]) * (kshape[2]//self.filter_size[1])])
-        kernel = tf.transpose(kernel, [1, 2, 0, 3, 4])
-        kernel = tf.reshape(kernel, [self.filter_size[0],
-                                     self.filter_size[1],
-                                     kshape[-1] * MB,
-                                     (kshape[1]//self.filter_size[0]) * (kshape[2]//self.filter_size[1])])
+#         kernel = tf.reshape(kernel, [MB,
+#                                      self.filter_size[0],
+#                                      self.filter_size[1],
+#                                      kshape[-1],
+#                                      (kshape[1]//self.filter_size[0]) * (kshape[2]//self.filter_size[1])])
+#         kernel = tf.transpose(kernel, [1, 2, 0, 3, 4])
+#         kernel = tf.reshape(kernel, [self.filter_size[0],
+#                                      self.filter_size[1],
+#                                      kshape[-1] * MB,
+#                                      (kshape[1]//self.filter_size[0]) * (kshape[2]//self.filter_size[1])])
         
-        feature_map_2 = tf.transpose(feature_map_2, [1, 2, 0, 3]) # shape (H, W, MB, channels_img)
-        feature_map_2 = tf.reshape(feature_map_2, [1, shape[1], shape[2], MB*shape[3]])
-        feature_map_2 = tf.cast(feature_map_2, dtype=dtype)
+#         feature_map_2 = tf.transpose(feature_map_2, [1, 2, 0, 3]) # shape (H, W, MB, channels_img)
+#         feature_map_2 = tf.reshape(feature_map_2, [1, shape[1], shape[2], MB*shape[3]])
+#         feature_map_2 = tf.cast(feature_map_2, dtype=dtype)
         
         
-        out = tf.nn.depthwise_conv2d(feature_map_2,
-                                     kernel,
-                                     strides=[1, self.strides[0], self.strides[1], 1],
-                                     padding='SAME')
-        out = tf.cast(out, dtype=dtype)
-        out = tf.reshape(out, [shape[1]//self.filter_size[0],
-                               shape[2]//self.filter_size[1],
-                               MB,
-                               shape[3], 
-                               (kshape[1]//self.filter_size[0]) * (kshape[2]//self.filter_size[1])]) # careful about the order of depthwise conv out_channels!
-        out = tf.transpose(out, [2, 0, 1, 3, 4])
-        out = tf.reduce_sum(out, axis=3)
-        out = tf.reshape(out, [MB,
-                               (static_shape[1] - self.filter_size[0])//(self.strides[0]) + 1,
-                               (static_shape[2] - self.filter_size[1])//(self.strides[1]) + 1,
-                               (kshape[1] // self.filter_size[0]) * (kshape[2] // self.filter_size[1])])
+#         out = tf.nn.depthwise_conv2d(feature_map_2,
+#                                      kernel,
+#                                      strides=[1, self.strides[0], self.strides[1], 1],
+#                                      padding='SAME')
+#         out = tf.cast(out, dtype=dtype)
+#         out = tf.reshape(out, [shape[1]//self.filter_size[0],
+#                                shape[2]//self.filter_size[1],
+#                                MB,
+#                                shape[3], 
+#                                (kshape[1]//self.filter_size[0]) * (kshape[2]//self.filter_size[1])]) # careful about the order of depthwise conv out_channels!
+#         out = tf.transpose(out, [2, 0, 1, 3, 4])
+#         out = tf.reduce_sum(out, axis=3)
+#         out = tf.reshape(out, [MB,
+#                                (static_shape[1] - self.filter_size[0])//(self.strides[0]) + 1,
+#                                (static_shape[2] - self.filter_size[1])//(self.strides[1]) + 1,
+#                                (kshape[1] // self.filter_size[0]) * (kshape[2] // self.filter_size[1])])
 
-        if self.upsample:
-            out = tf.image.resize(out, (static_shape[1],static_shape[2]))
-            out = tf.reshape(out, [MB,
-                                  static_shape[1],
-                                  static_shape[2],
-                                   (kshape[1] // self.filter_size[0]) * (kshape[2] // self.filter_size[1])])
-            out = tf.cast(out, dtype=dtype)
-        if self.preserve_depth:
-            out = self.conv(out)
+#         if self.upsample:
+#             out = tf.image.resize(out, (static_shape[1],static_shape[2]))
+#             out = tf.reshape(out, [MB,
+#                                   static_shape[1],
+#                                   static_shape[2],
+#                                    (kshape[1] // self.filter_size[0]) * (kshape[2] // self.filter_size[1])])
+#             out = tf.cast(out, dtype=dtype)
+#         if self.preserve_depth:
+#             out = self.conv(out)
+        out = tf.reshape(kernel, kshape)
         out = tf.cast(out, dtype=dtype)
         return out
 
