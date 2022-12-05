55ff71e2417a53e70e72650495918cc7aa0b2270
Javad <54026703+JVD9kh96@users.noreply.github.com>
2022-12-05 23:35:26 +0330

    Update common.py

--- core/common.py
+++ core/common.py
@@ -182,9 +182,9 @@ class conv_prod(tf.keras.layers.Layer):
 #                            strides=[1, self.strides[0], self.strides[1], 1],
 #                            rates=[1, 1, 1, 1],
 #                            padding='SAME')
-#         kernel = patch_extractor((self.filter_size[0], self.filter_size[1]))(feature_map_1)
+        kernel = patch_extractor((self.filter_size[0], self.filter_size[1]))(feature_map_1)
         
-        kernel = feature_map_1
+#         kernel = feature_map_1
         
         #kernel = tf.transpose(kernel, perm=[0, 3, 1, 2])
         
