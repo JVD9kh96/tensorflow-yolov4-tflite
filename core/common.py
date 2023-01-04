461295c275c062614457698b3449fd73f7134b90
Javad <54026703+JVD9kh96@users.noreply.github.com>
2023-01-04 18:39:52 +0330

    Update common.py

--- core/common.py
+++ core/common.py
@@ -516,7 +516,7 @@ def kai_attention(key,
 #     qk    = tf.einsum('aijb,ajkb->aikb', query, key)/tf.math.sqrt(dk)
 #    qk    = tf.multiply(query, key)
 
-    qk = conv_prod(filter_size=[query.shape[1]//16,query.shape[1]//16], strides=[query.shape[1]//16,query.shape[1]//16],upsample=False, preserve_depth=True)(query, key)
+    qk = conv_prod(filter_size=[query.shape[1]//16, query.shape[1]//16], strides=[query.shape[1]//16, query.shape[1]//16], upsample=False, preserve_depth=True)(query, key)
 #     qk = conv_prod(filter_size=[2, 2], strides=[2, 2],upsample=False, preserve_depth=True)(query, key)
     if normalization == 'batch':
         qk = tf.keras.layers.experimental.SyncBatchNormalization()(qk)
@@ -544,7 +544,7 @@ def kai_attention(key,
 #         qk = softmax_2d()(qk)
     qk        = tf.nn.sigmoid(qk)
 #    attention = tf.math.multiply(qk , value)
-    attention = conv_prod(filter_size=[qk.shape[1]//16,qk.shape[1]//16], strides=[qk.shape[1]//16,qk.shape[1]//16],upsample=True, preserve_depth=True)(qk, value)
+    attention = conv_prod(filter_size=[qk.shape[1]//16,qk.shape[1]//16], strides=[qk.shape[1]//16,qk.shape[1]//16],upsample=True, preserve_depth=True)(qk, value) + value
 #     attention = conv_prod(filter_size=[2,2], strides=[2,2],upsample=True, preserve_depth=True)(qk, value)
     
     attention = tf.keras.layers.Conv2D(filters = out_filters//2, kernel_size = (1, 1), strides = (1, 1), padding = 'same',
