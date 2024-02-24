b55772efb676474ab072bb22ecea2be7108e7e55
Javad <54026703+JVD9kh96@users.noreply.github.com>
2024-02-24 04:35:39 +0330

    Update common.py

--- core/common.py
+++ core/common.py
@@ -219,12 +219,14 @@ def residual_block(input_layer, input_channel, filter_num1, filter_num2, activat
 def route_group(input_layer, groups, group_id):
     convs = tf.split(input_layer, num_or_size_splits=groups, axis=-1)
     return convs[group_id]
+
 class Upsample(tf.keras.layers.Layer):
-    def call(x, dt=None, training=False):
-        if dt is None:
-            dtype = input_layer.dtype
-        else:
-            dtype = dt
+    def call(x, training=False):
+        # if dt is None:
+        #     dtype = input_layer.dtype
+        # else:
+        #     dtype = dt
+        dtype = input_layer.dtype
         return tf.cast(tf.image.resize(input_layer, (input_layer.shape[1] * 2, input_layer.shape[2] * 2), method='bilinear'), dtype)
         
 # def upsample(input_layer, dtype=None):
